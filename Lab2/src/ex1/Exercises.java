4899b8c9b8f9a2f8d31dbbac94f39e9d6d9817c5
gichul0317 <gichul0317@gmail.com>
2026-02-11 06:23:24 -0500

    lab2 exercise 1 updated

--- Lab2/src/ex1/Exercises.java
+++ Lab2/src/ex1/Exercises.java
@@ -34,50 +34,80 @@ class Exercises {
 
   /** Returns the sum of the integers in given array. */
   public static int example1(int[] arr) {
-    int n = arr.length, total = 0;
-    for (int j=0; j < n; j++)       // loop from 0 to n-1
-      total += arr[j];
-    return total;
+    int n = arr.length, total = 0;  // n = size of the array (input size)
+    for (int j=0; j < n; j++)       // loop from 0 to n-1 (loop runs n times)
+      total += arr[j];              // operate in each iteration
+    return total;                   // return final total value
   }
+  /*
+  O(n)
+  - loop iterates each n times
+  - running time grows as n grows
+  - running time grows linearly as n grows
+  */
 
   /** Returns the sum of the integers with even index in given array. */
   public static int example2(int[] arr) {
-    int n = arr.length, total = 0;
+    int n = arr.length, total = 0;  // n = size of the array (input size)
     for (int j=0; j < n; j += 2)    // note the increment of 2
-      total += arr[j];
-    return total;
+      total += arr[j];              // operate in each iteration (increment by 2)
+    return total;                   // return final total value
   }
+  /*
+   O(n)
+   - loop iterates each n times (only in increment of 2)
+   - as same as above method, running time grows linearly as n grows
+   */
 
   /** Returns the sum of the prefix sums of given array. */
   public static int example3(int[] arr) {
-    int n = arr.length, total = 0;
+    int n = arr.length, total = 0;  // n = size of the array (input size)
     for (int j=0; j < n; j++)       // loop from 0 to n-1
       for (int k=0; k <= j; k++)    // loop from 0 to j
-        total += arr[j];
-    return total;
+        total += arr[j];            // operate in each iteration
+    return total;                   // return final total value
   }
+  /*
+  O(n^2)
+  - outer loop runs n times
+  - inner loop runs j + 1 times
+  - n * n = n^2
+  - running time grows quadratically (n^2)
+   */
 
   /** Returns the sum of the prefix sums of given array. */
   public static int example4(int[] arr) {
-    int n = arr.length, prefix = 0, total = 0;
+    int n = arr.length, prefix = 0, total = 0; // n = size of the array (input size)
     for (int j=0; j < n; j++) {     // loop from 0 to n-1
-      prefix += arr[j];
-      total += prefix;
+      prefix += arr[j];             // operate in each iteration
+      total += prefix;              // operate in each iteration
     }
-    return total;
+    return total;                   // return final total value
   }
+  /*
+  O(n)
+  - loop iterates n times
+  - running time grows linearly as n grows
+  - it is not nested loop
+   */
 
   /** Returns the number of times second array stores sum of prefix sums from first. */
   public static int example5(int[] first, int[] second) { // assume equal-length arrays
-    int n = first.length, count = 0;
+    int n = first.length, count = 0;// n = size of the array (input size)
     for (int i=0; i < n; i++) {     // loop from 0 to n-1
-      int total = 0;
+      int total = 0;                // initial value of total = 0
       for (int j=0; j < n; j++)     // loop from 0 to n-1
         for (int k=0; k <= j; k++)  // loop from 0 to j
-          total += first[k];
-      if (second[i] == total) count++;
+          total += first[k];        // operate in each iteration
+      if (second[i] == total) count++; // check elements in second array with total value then increase count
     }
-    return count;
+    return count;                   // return final count value
   }
-
+  /*
+  O(n^3)
+  - outer loop runs n times
+  - middle loop runs n times
+  - inner loop runs j + 1 times
+  - total run time = n * n * n= n^3
+   */
 }
