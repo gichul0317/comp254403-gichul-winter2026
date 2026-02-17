c012b4bb43df43ee0e2c8ffad29b1491a0803fdf
gichul0317 <gichul0317@gmail.com>
2026-02-17 18:51:03 -0500

    created exercise 1 for the lab 3

---
+++ Lab3/src/ex1/Exercise1.java
@@ -0,0 +1,13 @@
+package ex1;
+
+public class Exercise1 {
+    // recursive algorithm to compute product of two positive integers m and n
+    public static int productOfTwo(int m, int n) {
+        return m * n;
+    }
+
+    // main method for testing
+    public static void main(String[] args) {
+
+    }
+}
