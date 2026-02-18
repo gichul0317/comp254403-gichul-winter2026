1c7ce8d9ad19be0fdc9751df9ad56c95ba0e7e02
gichul0317 <gichul0317@gmail.com>
2026-02-18 11:28:08 -0500

    created exercise 2 for the lab 3

---
+++ Lab3/src/ex2/Exercise2.java
@@ -0,0 +1,13 @@
+package ex2;
+
+public class Exercise2 {
+    // reverse the string?
+    public static String palindromeTest(String str) {
+        return str;
+    }
+
+    // main for testing
+    public static void main(String[] args) {
+
+    }
+}
