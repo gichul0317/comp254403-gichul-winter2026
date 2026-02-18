e0ad584922b5ee783d233187787323f50371c314
gichul0317 <gichul0317@gmail.com>
2026-02-18 11:43:08 -0500

    solving exercise 2 for the lab 3

--- Lab3/src/ex2/Exercise2.java
+++ Lab3/src/ex2/Exercise2.java
@@ -1,13 +1,22 @@
 package ex2;
 
 public class Exercise2 {
-    // reverse the string?
+    // reverse the string to check whether its palindrome or not
     public static String palindromeTest(String str) {
-        return str;
+        // if string length is less than 1
+        if (str.length() <= 1) {
+            return str;
+        }
+
+        // moving it's first character to the end and repeats it recursively
+        return palindromeTest(str.substring(1)) + str.charAt(0);
     }
 
     // main for testing
     public static void main(String[] args) {
+        String testStr = "hello";
+        String result = palindromeTest(testStr);
 
+        System.out.println(testStr + " After: "+ result);
     }
 }
