7cf1e3f1504b0444c89f12d712b625697af9c224
gichul0317 <gichul0317@gmail.com>
2026-02-18 13:01:45 -0500

    solving exercise 3 for the lab 3

--- Lab3/src/ex3/Exercise3.java
+++ Lab3/src/ex3/Exercise3.java
@@ -5,18 +5,18 @@ import java.io.File;
 public class Exercise3 {
     // find file path
     public static void find(String path, String filename) {
-        File file = new File(path);
-        System.out.println(filename);
-        System.out.println(file.getName()); // ex3
-        System.out.println("File path: " + file.getPath());
-        System.out.println("File path: " + file.getAbsolutePath());
+        File file = new File(path); // create File object
+
+        System.out.println();
+        System.out.println(file.getName());
+        System.out.println(file.getAbsoluteFile());
     }
 
     // main for testing
     public static void main(String[] args) {
-        String path = "/Users/gichul/Desktop/winter2026/comp254-algorithms/Lab3/src/ex3";
-        String file = "test.txt";
+        String path = "/Users/gichul/Desktop/winter2026/comp254-algorithms/Lab3/src/ex3";   // start path
+        String file = "test.txt";   // target file name
 
-        find(path, file);
+        find(path, file); // call find
     }
 }
