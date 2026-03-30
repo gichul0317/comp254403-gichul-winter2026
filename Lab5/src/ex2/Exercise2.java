05930c432974cfee1d85c3a9d21bd907cf99b32a
gichul0317 <gichul0317@gmail.com>
2026-03-30 17:44:44 -0400

    updated exercise 2 for assignment 5

--- Lab5/src/ex2/Exercise2.java
+++ Lab5/src/ex2/Exercise2.java
@@ -1,4 +1,20 @@
 package ex2;
 
 public class Exercise2 {
+    public static void main(String[] args) {
+        // create new LinkedBinaryTree
+        LinkedBinaryTree<String> tree = new LinkedBinaryTree<>();
+
+        // add nodes
+        Position<String> A = tree.addRoot("A");
+        Position<String> B = tree.addLeft(A, "B");
+        Position<String> C = tree.addRight(A, "C");
+        Position<String> D = tree.addLeft(B, "D");
+        Position<String> E = tree.addRight(B, "E");
+        Position<String> F = tree.addLeft(C, "F");
+        Position<String> G = tree.addRight(C, "G");
+
+        // print subtree height for each node
+        tree.printSubtreeHeights(tree.root());
+    }
 }
