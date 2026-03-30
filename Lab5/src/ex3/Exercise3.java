68ee1336221fd2f698afd6d3c7b8f85fe1af57ff
gichul0317 <gichul0317@gmail.com>
2026-03-30 17:58:35 -0400

    updated exercise 3 for assignment 5

--- Lab5/src/ex3/Exercise3.java
+++ Lab5/src/ex3/Exercise3.java
@@ -1,4 +1,20 @@
 package ex3;
 
 public class Exercise3 {
+    // main for testing
+    public static void main(String[] args) {
+        // create priority queue
+        HeapPriorityQueue<Integer, String> priorityQueue = new HeapPriorityQueue<>();
+
+        // insert key-value pairs
+        priorityQueue.insert(20, "A");
+        priorityQueue.insert(15, "B");
+        priorityQueue.insert(10, "C");
+        priorityQueue.insert(5, "D");
+
+        // remove smallest key first (priority)
+        while (!priorityQueue.isEmpty()) {
+            System.out.println(priorityQueue.removeMin().getKey());
+        }
+    }
 }
